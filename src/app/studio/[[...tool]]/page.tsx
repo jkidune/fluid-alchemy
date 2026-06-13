feaedb27b9a13fdc786af69a06a70d0627d219ab
Joseph Masonda <kidunejoseph91@gmail.com>
2026-06-13 19:07:05 +0300

    package.json

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,19 +1,11 @@
-/**
- * This route is responsible for the built-in authoring environment using Sanity Studio.
- * All routes under your studio path is handled by this file using Next.js' catch-all routes:
- * https://nextjs.org/docs/routing/dynamic-routes#catch-all-routes
- *
- * You can learn more about the next-sanity package here:
- * https://github.com/sanity-io/next-sanity
- */
-
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../../sanity.config'
+import { redirect } from 'next/navigation'
 
 export const dynamic = 'force-static'
 
-export { metadata, viewport } from 'next-sanity/studio'
+export const metadata = {
+  title: 'Fluid Alchemy Studio',
+}
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  redirect(process.env.NEXT_PUBLIC_SANITY_STUDIO_URL || 'https://www.sanity.io/manage')
 }
